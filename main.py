722bff964c048f7b1713da6650a98c12c19a6bf2
Jacques Légaré <jacques.legare70@gmail.com>
2020-01-13 11:20:57 -0500

    Parse an XML document and collect specified attributes.

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+from lxml import etree
+
+def main (file_name):
+    parser = etree.XMLParser ()
+    tree   = etree.parse (file_name, parser)
+
+    walk (tree.getroot (), 0)
+
+
+def walk (element, indentation):
+    print (" " * indentation + element.tag)
+
+    for ( name, value ) in sorted (element.items ()):
+        print (" " * (indentation + 3) + ":" + name + " = \"" + value + "\"")
+
+    for child in element:
+        walk (child, indentation + 3)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main (sys.argv[1])
+
+    else:
+        print ("ERROR: An input file name must be specified on the command-line.")
+
+
